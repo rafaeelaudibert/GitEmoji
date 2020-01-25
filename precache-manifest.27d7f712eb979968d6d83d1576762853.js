e3deab4a256e0483a99e4402a124fd7e1fde6fe2
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2020-01-25 01:32:46 -0200

    Updates

---
+++ precache-manifest.27d7f712eb979968d6d83d1576762853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d4e149d0f1fc77d74bfe3a7813a8b9",
+    "url": "/GitEmoji/index.html"
+  },
+  {
+    "revision": "70a33cbc92cff145a7f9",
+    "url": "/GitEmoji/static/css/main.5e07143f.chunk.css"
+  },
+  {
+    "revision": "ec2fb74fb22005a9dc0c",
+    "url": "/GitEmoji/static/js/2.e38d832c.chunk.js"
+  },
+  {
+    "revision": "1616c567d6e21e18bd2baf079b391cc4",
+    "url": "/GitEmoji/static/js/2.e38d832c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70a33cbc92cff145a7f9",
+    "url": "/GitEmoji/static/js/main.46963c0a.chunk.js"
+  },
+  {
+    "revision": "3ab334b473f92ca9b288",
+    "url": "/GitEmoji/static/js/runtime-main.b29566cf.js"
+  },
+  {
+    "revision": "3fe6a472d5a8737e79de0e69cab7e12c",
+    "url": "/GitEmoji/static/media/logo.3fe6a472.png"
+  }
+]);
